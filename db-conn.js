45ec35b5e8365413d4883282aa38bef95db240b0
tahrima42 <tahrima.ihsan@northsouth.edu>
2021-05-07 19:53:24 +0600

    data inserted

---
+++ db-conn.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'demodb'
+});
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected to database successfully');
+});
+
+module.exports = connection;
